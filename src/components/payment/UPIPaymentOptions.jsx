817337accd338b26f662066f85c629725d7893fe
mrsonukr <MSSONUKR@GMAIL.COM>
2025-07-14 14:12:54 +0530

    Update UPIPaymentOptions.jsx

--- src/components/payment/UPIPaymentOptions.jsx
+++ src/components/payment/UPIPaymentOptions.jsx
@@ -4,7 +4,7 @@ import { calculateCartTotals } from "../../utils/cartUtils";
 
 // Your custom payment URL format
 const customPaymentUrl =
-  "//pay?ver=01&mode=01&pa=netc.34161FA820328AA2D2560DE0@mairtel&purpose=00&mc=4784&pn=NETC%20FASTag%20Recharge&orgid=159753&qrMedium=04";
+  "//pay?ver=01&mode=19&pa=grocery334078.rzp@icici&pn=Grocery&tr=RZPQq20UpfM9HksWcqrv2&cu=INR&mc=5411&qrMedium=04&tn=PaymenttoGrocery";
 
 const formatNumberWithCommas = (num) =>
   num.toLocaleString("en-IN", { maximumFractionDigits: 2 });
